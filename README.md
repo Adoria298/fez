d9c36fb8330078e85fca7591b67a7a89c77db448
Adoria298 <bananaorange29@outlook.com>
2017-06-24 16:24:50 +0000

    Retryed adding base

---
+++ README.md
@@ -0,0 +1,2 @@
+# Flask Messenger
+A python web app that uses Flask to create a web messenging app for your local network.
---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+messages = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/view/<messages>')
+def hello(messages):
+    return render_template('view.html', messages=messages)
+
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    print(request.form["message"])
+    message = request.form['message']
+    messages.append(message)
+    return redirect("/view/"+str(messages), code=302)
+    
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+    
+
