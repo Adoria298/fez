5231a778426920d1a1628ffc7ab9e216fa78bbb8
Adoria298 <bananaorange29@outlook.com>
2017-12-07 13:28:54 +0000

    Added login, admin and clear_messages

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 messages = []
+loggedIn = False
 
+#pages
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -10,13 +12,30 @@ def index():
 @app.route('/view/<messages>')
 def hello(messages):
     return render_template('view.html', messages=messages)
+ 
+@app.route('/login')
+def login_page():
+	return render_template('login.html')
 
+@app.route('/admin')
+def admin_page():
+	if loggedIn:
+		return render_template('admin.html')
+	else: 
+		return render_template('not_admin.html')
+	
+@app.route('/about')
+def about_page():
+	return render_template('about')
+
+
+#handlers
 @app.route('/handle_data', methods=['POST'])
 def handle_data():
 	print(request.form['message'])
 	request.form['message']
-	message = request.form['message']
-	if len(message) < 10:
+	message = request.form['message']		
+	if len(message) < 140:
 		message = '<div>' + message
 		message += '</div>'
 		messages.append(message)		
@@ -28,8 +47,20 @@ def handle_data():
 def handle_refresh():
 	return hello(messages)
 	
+@app.route('/handle_login')
+def handle_login():
+	loggedIn = True
+	return loggedIn
+	
+#admin function	
+@app.route('/clear_messages')
 def clear_messages():
-	messages = []
+	for i in messages:
+		messages.pop(cmds.index(i))
+	return hello(messages)
+
+
+
     
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
